a7f517ff6999384a08f051b64dc3c4d8db9ba139
Yulia Gurkina <lyulyamir@gmail.com>
2021-02-14 19:27:08 +0300

    Компоненты для всех страниц приложения; react-router-dom; маршруты

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Main from "../main/main";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import Login from "../login/login";
+import Favorites from "../favorites/favorites";
+import Property from "../property/property";
+import NotFound from "../not-found/not-found";
 
 const App = (props) => {
   return (
-    <Main offersCount={props.offersCount}/>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Main offersCount={props.offersCount}/>
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/favorites">
+          <Favorites />
+        </Route>
+        <Route exact path="/offer/:id?">
+          <Property />
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
