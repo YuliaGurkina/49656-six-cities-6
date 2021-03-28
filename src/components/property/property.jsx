e7c90fe15806cad31704cb5945e1145eb3eb2194
Yulia Gurkina <lyulyamir@gmail.com>
2021-03-28 10:55:05 +0300

    блок «Other places in the neighbourhood»

--- src/components/property/property.jsx
+++ src/components/property/property.jsx
@@ -6,10 +6,116 @@ import {fetchCommentsList} from "../../store/api-actions";
 import {useDispatch, useSelector} from "react-redux";
 import PropTypes from "prop-types";
 import {getComments} from "../../selectors";
+import Map from "../map/map";
+import OfferList from "../offer-list/offer-list";
 
 const Property = ({id}) => {
   const dispatch = useDispatch();
   const comments = useSelector((state) => getComments(state));
+  const mockOffers = [
+    {
+      "bedrooms": 3,
+      "city": {
+        "location": {
+          "latitude": 52.370216,
+          "longitude": 4.895168,
+          "zoom": 10
+        },
+        "name": `Amsterdam`
+      },
+      "description": `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
+      "goods": [`Heating`, `Kitchen`, `Cable TV`, `Washing machine`, `Coffee machine`, `Dishwasher`],
+      "host": {
+        "avatar_url": `img/1.png`,
+        "id": 3,
+        "is_pro": true,
+        "name": `Angelina`
+      },
+      "id": 1,
+      "images": [`img/1.png`, `img/2.png`],
+      "isFavorite": false,
+      "isPremium": false,
+      "location": {
+        "latitude": 52.35514938496378,
+        "longitude": 4.673877537499948,
+        "zoom": 8
+      },
+      "max_adults": 4,
+      "previewImage": `img/apartment-03.jpg`,
+      "price": 120,
+      "rating": 4.8,
+      "title": `Beautiful & luxurious studio at great location`,
+      "type": `apartment`
+    },
+    {
+      "bedrooms": 1,
+      "city": {
+        "location": {
+          "latitude": 57.370216,
+          "longitude": 6.895168,
+          "zoom": 10
+        },
+        "name": `Amsterdam`
+      },
+      "description": `A quiet cozy and picturesque`,
+      "goods": [`Heating`, `Kitchen`, `Cable TV`, `Dishwasher`],
+      "host": {
+        "avatar_url": `img/1.png`,
+        "id": 21,
+        "is_pro": true,
+        "name": `Judi`
+      },
+      "id": 2,
+      "images": [`img/1.png`, `img/2.png`],
+      "isFavorite": true,
+      "isPremium": false,
+      "location": {
+        "latitude": 52.35414938496378,
+        "longitude": 4.673877537499948,
+        "zoom": 8
+      },
+      "max_adults": 2,
+      "previewImage": `img/apartment-01.jpg`,
+      "price": 120,
+      "rating": 4.8,
+      "title": `Beautiful & luxurious studio at great location`,
+      "type": `apartment`
+    },
+    {
+      "bedrooms": 2,
+      "city": {
+        "location": {
+          "latitude": 52.370216,
+          "longitude": 4.895168,
+          "zoom": 10
+        },
+        "name": `Amsterdam`
+      },
+      "description": `A quiet cozy river by the unique lightness of Amsterdam.`,
+      "goods": [`Heating`, `Kitchen`, `Cable TV`, `Washing machine`, `Coffee machine`, `Dishwasher`],
+      "host": {
+        "avatar_url": `img/1.png`,
+        "id": 22,
+        "is_pro": true,
+        "name": `Mike`
+      },
+      "id": 3,
+      "images": [`img/1.png`, `img/2.png`],
+      "isFavorite": false,
+      "isPremium": true,
+      "location": {
+        "latitude": 52.35314938496378,
+        "longitude": 4.653877537499948,
+        "zoom": 8
+      },
+      "max_adults": 3,
+      "previewImage": `img/apartment-02.jpg`,
+      "price": 120,
+      "rating": 4.8,
+      "title": `Beautiful & luxurious studio at great location`,
+      "type": `apartment`
+    },
+  ];
 
   useEffect(() => {
     dispatch(fetchCommentsList({hotelId: id}));
@@ -141,105 +247,17 @@ const Property = ({id}) => {
               </ReviewsList>
             </div>
           </div>
-          <section className="property__map map"/>
+          <section className="property__map map">
+            <Map
+              points={mockOffers}
+            />
+          </section>
         </section>
         <div className="container">
           <section className="near-places places">
             <h2 className="near-places__title">Other places in the neighbourhood</h2>
             <div className="near-places__list places__list">
-              <article className="near-places__card place-card">
-                <div className="near-places__image-wrapper place-card__image-wrapper">
-                  <a href="#">
-                    <img className="place-card__image" src="img/room.jpg" width="260" height="200" alt="Place image"/>
-                  </a>
-                </div>
-                <div className="place-card__info">
-                  <div className="place-card__price-wrapper">
-                    <div className="place-card__price">
-                      <b className="place-card__price-value">&euro;80</b>
-                      <span className="place-card__price-text">&#47;&nbsp;night</span>
-                    </div>
-                    <button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button">
-                      <svg className="place-card__bookmark-icon" width="18" height="19">
-                        <use xlinkHref="#icon-bookmark"/>
-                      </svg>
-                      <span className="visually-hidden">In bookmarks</span>
-                    </button>
-                  </div>
-                  <div className="place-card__rating rating">
-                    <div className="place-card__stars rating__stars">
-                      <span style={{width: `80%`}}/>
-                      <span className="visually-hidden">Rating</span>
-                    </div>
-                  </div>
-                  <h2 className="place-card__name">
-                    <a href="#">Wood and stone place</a>
-                  </h2>
-                  <p className="place-card__type">Private room</p>
-                </div>
-              </article>
-              <article className="near-places__card place-card">
-                <div className="near-places__image-wrapper place-card__image-wrapper">
-                  <a href="#">
-                    <img className="place-card__image" src="img/apartment-02.jpg" width="260" height="200" alt="Place image"/>
-                  </a>
-                </div>
-                <div className="place-card__info">
-                  <div className="place-card__price-wrapper">
-                    <div className="place-card__price">
-                      <b className="place-card__price-value">&euro;132</b>
-                      <span className="place-card__price-text">&#47;&nbsp;night</span>
-                    </div>
-                    <button className="place-card__bookmark-button button" type="button">
-                      <svg className="place-card__bookmark-icon" width="18" height="19">
-                        <use xlinkHref="#icon-bookmark"/>
-                      </svg>
-                      <span className="visually-hidden">To bookmarks</span>
-                    </button>
-                  </div>
-                  <div className="place-card__rating rating">
-                    <div className="place-card__stars rating__stars">
-                      <span style={{width: `80%`}}/>
-                      <span className="visually-hidden">Rating</span>
-                    </div>
-                  </div>
-                  <h2 className="place-card__name">
-                    <a href="#">Canal View Prinsengracht</a>
-                  </h2>
-                  <p className="place-card__type">Apartment</p>
-                </div>
-              </article>
-              <article className="near-places__card place-card">
-                <div className="near-places__image-wrapper place-card__image-wrapper">
-                  <a href="#">
-                    <img className="place-card__image" src="img/apartment-03.jpg" width="260" height="200" alt="Place image"/>
-                  </a>
-                </div>
-                <div className="place-card__info">
-                  <div className="place-card__price-wrapper">
-                    <div className="place-card__price">
-                      <b className="place-card__price-value">&euro;180</b>
-                      <span className="place-card__price-text">&#47;&nbsp;night</span>
-                    </div>
-                    <button className="place-card__bookmark-button button" type="button">
-                      <svg className="place-card__bookmark-icon" width="18" height="19">
-                        <use xlinkHref="#icon-bookmark"/>
-                      </svg>
-                      <span className="visually-hidden">To bookmarks</span>
-                    </button>
-                  </div>
-                  <div className="place-card__rating rating">
-                    <div className="place-card__stars rating__stars">
-                      <span style={{width: `100%`}}/>
-                      <span className="visually-hidden">Rating</span>
-                    </div>
-                  </div>
-                  <h2 className="place-card__name">
-                    <a href="#">Nice, cozy, warm big bed apartment</a>
-                  </h2>
-                  <p className="place-card__type">Apartment</p>
-                </div>
-              </article>
+              <OfferList offers={mockOffers} customCardClass='near-places__card' customCardImgClass='near-places__image-wrapper'/>
             </div>
           </section>
         </div>
